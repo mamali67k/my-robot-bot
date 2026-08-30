0f81b3d884b5b33606fcd44b3b2d0cda2e48bc7e
mamali67k <63202230+mamali67k@users.noreply.github.com>
2026-08-30 17:58:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-# main.py
-import json
+import os
 import logging
 from telegram.ext import ApplicationBuilder, CommandHandler
-import config # ایمپورت کردن توکن از فایل config
 
 # تنظیم لاگینگ برای عیب‌یابی در سرور
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 async def start(update, context):
     await update.message.reply_text("سلام! ربات فروشگاهی هوشمند شما فعال شد.")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(config.TOKEN).build()
+    # خواندن توکن از Environment Variable در Railway
+    TOKEN = os.getenv("BOT_TOKEN")
+
+    if not TOKEN:
+        raise ValueError("❌ BOT_TOKEN در Variables تنظیم نشده است!")
+
+    app = ApplicationBuilder().token(TOKEN).build()
     
     app.add_handler(CommandHandler("start", start))
     
-    print("ربات با موفقیت استارت خورد...")
+    print("✅ ربات با موفقیت استارت خورد...")
     app.run_polling()
